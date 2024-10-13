47cfb5f7a014e1e21f9ca8c573eed0b53265bcda
Kyrylo Polovko <kiriushkapolovko@gmail.com>
2024-10-13 14:45:57 +0900

    backend changes
    added new endpoint for serving .zip file with media, implemented a function which extracts media from Message object and zips all the scraped media

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import PlainTextResponse, RedirectResponse
+from fastapi.responses import PlainTextResponse, RedirectResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
@@ -7,10 +7,13 @@ from telethon.sync import TelegramClient
 from telethon.errors import ChannelInvalidError, ChannelPrivateError
 from enum import Enum
 from csv import DictWriter
+from uuid import uuid4
+from zipfile import ZipFile
 import os
 import json
 import uvicorn
 from io import StringIO
+import shutil
 
 load_dotenv()
 api_id = os.getenv('API_ID')
@@ -44,16 +47,18 @@ async def scrape_channel(channel_name: str, format, limit: int | None = None, do
         entity = await client.get_entity(channel_name)
         
         messages = []
+        obj_messages = []
         async for message in client.iter_messages(entity, limit=limit):
+            obj_messages.append(message)
             messages.append({
                 "message": message.text,
                 "id": message.id,
                 "date": message.date.isoformat()
             })
-
-            if download_media and message.media:
-                await message.download_media(file=f'media/{message.id}')
         
+        if download_media:
+            await get_media(obj_messages)
+
         if not messages:
             raise HTTPException(status_code=404, detail="No messages found.")
         
@@ -69,6 +74,10 @@ async def scrape_channel(channel_name: str, format, limit: int | None = None, do
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    
+@app.get('/dowload_media_files', response_class=FileResponse)
+async def download_media_files():
+    return FileResponse('media.zip', media_type='application/zip')
         
 def format_content(content: list, format: str):
     match format:
@@ -82,5 +91,17 @@ def format_content(content: list, format: str):
                 writer.writerows(content)
                 return string_stream.getvalue()
 
+async def get_media(messages):
+    if not os.path.isdir('media'):
+        os.mkdir('media')
+    for message in messages:
+        await message.download_media(file='media')
+
+    with ZipFile('media.zip', 'w') as zip_object:
+        for file in os.listdir('media'):
+            zip_object.write(os.path.join('media', file))
+
+    shutil.rmtree('media')
+    
 if __name__=='__main__':
-    uvicorn.run("main:app", host="192.168.100.134", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
